f5dfb60b93819e7440d9505a7714cf722be60cd5
Aadil Glasswala <aglasswala@gmail.com>
2018-09-28 12:43:50 -0400

    Almost done with Components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Navigation from './Components/Navigation/Navigation';
+import Logo from './Components/Logo/Logo'
+import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
+import Rank from './Components/Rank/Rank';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Navigation /> 
+        <Logo /> 
+        <Rank />
+        <ImageLinkForm />{ /*
+        <FaceRecognition />
+         */ }
       </div>
     );
   }
---
+++ src/Components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const Rank = () => {
+	return (
+		<div>
+			<div className='white f3'>
+				{'Dilly, you\'re ranked ... ' }
+			</div>
+			<div className='white f1'>
+				{'#5'}
+			</div>
+		</div>
+	);
+}
+export default Rank;

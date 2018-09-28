a491e1fc6a34c56ba0316102eb7a19c7c3a8ba22
Aadil Glasswala <aglasswala@gmail.com>
2018-09-28 15:26:54 -0400

    Added more components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
+import Particles from 'react-particles-js';
 import Navigation from './Components/Navigation/Navigation';
-import Logo from './Components/Logo/Logo'
+import Logo from './Components/Logo/Logo';
 import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
 import Rank from './Components/Rank/Rank';
 import './App.css';
+
+const particleOptions = {
+                particles: {
+                  value: 30,
+                  density: {
+                    enable:true,
+                    value_area: 800
+                  }
+                }
+              }
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Particles className='particles'
+              params={particleOptions}
+            />
         <Navigation /> 
         <Logo /> 
         <Rank />
